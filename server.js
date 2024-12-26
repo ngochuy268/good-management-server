76d5b28daffeb53237b35782ad344f4f078d0447
ngochuy268 <75684058+ngochuy268@users.noreply.github.com>
2024-12-26 08:20:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(bodyParser.json({ limit: '100mb' }));
 
